1783723b1367ee9357e3b99fdf37464fcf48ca87
Andriana Popp <115152248+andrianapopp@users.noreply.github.com>
2023-04-25 19:49:26 +0300

    26npm

--- scripts/app.js
+++ scripts/app.js
@@ -1,76 +1,80 @@
 //indexOf
-function indexOf (arrIndexOf, pointOf){
-    for (let i = 0; i < arrIndexOf.length; i++){
-        if (arrIndexOf[i] === pointOf){
-            return i
-        }
-        return -1
+function indexOf(arrIndexOf, pointOf) {
+  for (let i = 0; i < arrIndexOf.length; i++) {
+    if (arrIndexOf[i] === pointOf) {
+      return i;
     }
+    return -1;
+  }
 }
-indexOf()
+indexOf();
 
 //lastIndexOf
-function  lastIndexOf (arrLastIndexOf, pointLast, indexLast = arrLastIndexOf - 1){
-    for (let i = indexLast; i >= 0; i--){
-        if (arrLastIndexOf[i] === pointLast){
-            return i
-        }
-        return -1
+function lastIndexOf(
+  arrLastIndexOf,
+  pointLast,
+  indexLast = arrLastIndexOf - 1
+) {
+  for (let i = indexLast; i >= 0; i--) {
+    if (arrLastIndexOf[i] === pointLast) {
+      return i;
     }
+    return -1;
+  }
 }
-lastIndexOf()
+lastIndexOf();
 
 //find
-function find (arrFind, findBack){
-    for (let i = 0; i < arrFind.length; i++){
-        if (findBack(arrFind[i], arrFind, findBack)){
-            return i
-        }
-        return undefined
+function find(arrFind, findBack) {
+  for (let i = 0; i < arrFind.length; i++) {
+    if (findBack(arrFind[i], arrFind, findBack)) {
+      return i;
     }
+    return undefined;
+  }
 }
-find()
+find();
 
 //findIndex
-function findIndex (arrFindIndex, findIndexBack){
-    for (let i = 0; i < arrFindIndex.length; i++){
-        if (findIndexBack(arrFindIndex[i], arrFindIndex, findIndexBack)){
-            return i
-        }
-        return -1
+function findIndex(arrFindIndex, findIndexBack) {
+  for (let i = 0; i < arrFindIndex.length; i++) {
+    if (findIndexBack(arrFindIndex[i], arrFindIndex, findIndexBack)) {
+      return i;
     }
+    return -1;
+  }
 }
-findIndex()
+findIndex();
 
 //some
-function some (arrSome, someBack){
-    for (let i = 0; i < arrSome.length; i++){
-        if (someBack(arrSome[i], someBack, arrSome)){
-            return true
-        }
-        return false
+function some(arrSome, someBack) {
+  for (let i = 0; i < arrSome.length; i++) {
+    if (someBack(arrSome[i], someBack, arrSome)) {
+      return true;
     }
+    return false;
+  }
 }
-some()
+some();
 
 //includes
-function includes (arrIncludes, pointIncludes){
-    for (let i = 0; i < arrIncludes.length; i++){
-        if (arrIncludes[i] === pointIncludes){
-            return true
-        }
-        return false
+function includes(arrIncludes, pointIncludes) {
+  for (let i = 0; i < arrIncludes.length; i++) {
+    if (arrIncludes[i] === pointIncludes) {
+      return true;
     }
+    return false;
+  }
 }
-includes()
+includes();
 
 //every
-function every (arrEvery, everyBack){
-    for (let i = 0; i < arrEvery.length; i++){
-        if (!everyBack(arrEvery[i], arrEvery, everyBack)){
-            return true
-        }
-        return false
+function every(arrEvery, everyBack) {
+  for (let i = 0; i < arrEvery.length; i++) {
+    if (!everyBack(arrEvery[i], arrEvery, everyBack)) {
+      return true;
     }
+    return false;
+  }
 }
-every()
+every();
